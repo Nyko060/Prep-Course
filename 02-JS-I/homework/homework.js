bd21dace32b8bb2d4970fd69bd9b0f03ba5d26bf
Nyko060 <nicolasromano981@gmail.com>
2022-08-02 12:52:29 -0300

    Suma

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {  
   // "Return" la string provista: str
   // Tu código:
-
+  return str;
 }
 
 function suma(x, y) {
@@ -41,27 +41,38 @@ function suma(x, y) {
 }
 
 function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
+  // Resta "y" de "x" y devuelve el valor 
   // Tu código:
-  
+  resta=x-y
+  return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  multiplica=x*y
+  return multiplica;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  divide=x/y
+  return divide;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(x==y)
+  { 
+    return true;
+  }
+  else
+  {
+    return false;
+  }
   
 }
 
