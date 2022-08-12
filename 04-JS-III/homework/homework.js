cb7747007709055e8b80e583b808c610929ed9f2
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 19:39:53 -0300

    Ejercicios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -68,6 +68,17 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for(i=0;i>=array.length-1;i++)
+  {
+    if(array[i]===elemento)
+    {
+        return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 }
 
 
