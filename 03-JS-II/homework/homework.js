b3685802165cc0253b6cef5db0ca93de3f930020
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 15:42:02 -0300

    Ejercicios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,7 +150,7 @@ function operadoresLogicos(num1, num2, num3) {
   }else if(num1<0||num2<0||num3<0){
     return 'Hay negativos';
   }else if(num1>num2&&num1>num3&&num1>0){
-    return 'Numero 1 es mayor y positivo';
+    return 'Número 1 es mayor y positivo';
   }else if(num3>num1&&num3>num2){
     return num3+1;
   }else{return false;}
