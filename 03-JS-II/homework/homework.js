b90e4da7162851e4626fb274633e815e88c34d32
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 17:42:54 -0300

    Ejercicios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -196,7 +196,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero.length>=3)
+  if(numero<1000)
   {
     return true;
   }
