2770c0083015f08b4eb2e0cd066542d05bb35f39
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 19:03:03 -0300

    Ejercicios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -38,6 +38,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+   return array;
+
 }
 
 
