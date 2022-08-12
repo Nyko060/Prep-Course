23f221f1222ce9fa30f6a640b53cbb04007e9c6e
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 13:46:52 -0300

    Ejercicios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -33,6 +33,14 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  switch(status) 
+  {
+    case 1: return 'Online';
+    break;
+    case 2: return 'Away';
+    break;
+    default: return 'Offline';
+  }
 }
 
 function saludo(idioma) {
