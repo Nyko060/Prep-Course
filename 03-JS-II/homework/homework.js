9ca4301dd32fb3284c2c9234ca473206993a88c3
Nyko060 <nicolasromano981@gmail.com>
2022-08-12 14:34:19 -0300

    Ejercicios

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -70,18 +70,38 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color) 
+  {
+    case 'blue': return 'This is blue';
+    break;
+    case 'red': return 'This is red';
+    break;
+    case 'green': return 'This is green';
+    break;
+    case 'orange': return 'This is orange';
+    break;
+    default: return 'Color not found';
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero===10 || numero===5)
+  {
+    return true;
+  }else{return false;}
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if((numero<50)&&(numero>20))
+  {
+    return true;
+  }else{return false;}
 }
 
 function esEntero(numero) {
@@ -92,6 +112,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if(numero===Math.floor(numero))
+  {
+    return true;
+  }else{return false;}
 }
 
 function fizzBuzz(numero) {
